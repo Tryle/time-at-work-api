093c42fef96c35f33eba372038d76e4e0e3c65fe
Damien Balthassat <damien.balthassat@gmail.com>
2015-01-24 10:07:38 +0100

    init django rest framework

--- time-at-work-api/settings.py
+++ time-at-work-api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,6 +53,13 @@ ROOT_URLCONF = 'time-at-work-api.urls'
 
 WSGI_APPLICATION = 'time-at-work-api.wsgi.application'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
